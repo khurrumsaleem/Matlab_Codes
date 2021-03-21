d950d1cb0e1ad83e34c78953853a1fd087a4d042
ibrivasortiz92 <78561871+ibrivasortiz92@users.noreply.github.com>
2021-03-21 01:39:30 -0300

    Initial commit

---
+++ PROBLEMS-1D/problem_10.m
@@ -0,0 +1,27 @@
+% PROBLEM NO. 1 (REF: LARSEN_TTSP_1986)
+
+  % QUADRATURE ORDER
+  N = 2;
+
+  % NUMBER OF ZONES
+  NZ = 1;  
+
+  ZON = [1.00;    % SIGMA_T
+         0.97];   % SIGMA_S
+
+  % NUMBER OF REGIONS
+  NR = 1;   
+
+  DOM = [100;     % LENGHT
+         100;     % NUMBER OF NODES
+           1;     % ZONE INDEX
+         0.0];    % INTERNAL SOURCE
+
+  % BOUNDARY CONDITIONS
+  BC = [1;        % LEFT
+        0];       % RIGHT
+    
+  % TOLERANCE
+  TOL = 1e-06;
+    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
