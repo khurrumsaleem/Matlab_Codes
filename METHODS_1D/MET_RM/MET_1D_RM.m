3347709e9b98afa1d0c6d7c1abb02ed369e99945
ibrivasortiz92 <78561871+ibrivasortiz92@users.noreply.github.com>
2021-03-23 09:25:19 -0300

    Implementation of the RM method 1D

---
+++ METHODS_1D/MET_RM/MET_1D_RM.m
@@ -0,0 +1,108 @@
+% IMPLEMENTATION OF THE RESPONSE MATRIX METHOD FOR FIXED SOURCE, ISOTROPIC 
+% SCATTERING, ONE-GROUP OF ENERGY DISCRETE-ORDINATES PROBLEMS IN SLAB
+% GEOMETRY.
+
+% INPUT: (PROBLEM SETUP)
+%        N   = QUADRATURE ORDER
+%        ZON = ZONE PARAMETERS
+%        DOM = DOMAIN PARAMETERS
+%        BC  = BOUNDARY CONDITIONS
+%        TOL = TOLERANCE (STOP CRITERIA)
+
+% OUTPUT: SFLUX = SCALAR FLUX AT THE EDGES
+%         AFLUX = ANGULAR FLUX AT THE EDGES
+%         ITER = NUMBER OF ITERATIONS
+%         TIME = CALCULATION TIME
+
+function [SFLUX, AFLUX, ITER, TIME] = MET_1D_RM(N, ZON, DOM, BC, TOL)
+
+  % VARIABLES
+  NR = length(DOM(1, :)); ntc = sum(DOM(2, :)); QUAD = GAUSS_QUAD(N);
+  [R, P] = response_matrix(QUAD, ZON, DOM);
+  err = 1; ITER = 0;
+
+  % INICIALIZATION
+  AFLUX = zeros(ntc + 1, N); SFLUX = zeros(ntc, 1);
+  for m = 1: N
+    if (m <= N/2 && BC(1) ~= -1)
+      AFLUX(1, m) = BC(1);
+    elseif (BC(2) ~= -1)
+      AFLUX(ntc + 1, m) = BC(2);
+    end
+  end
+  
+  % ITERATIVE PROCESS
+  tic;
+  fprintf("ITER\t\tMAX_DEV(SFLUX)\n");
+  while err > TOL
+    err = 0; ITER = ITER + 1;
+    
+    % 1. LEFT -> RIGHT SWEEP
+    IB = 0;
+    for r = 1: NR
+      nc = DOM(2, r);
+      RM = R(:, :, r); PM = P(:, r);
+      for i = 1: nc
+        IB = IB + 1; IF = IB + 1;
+        IN = [AFLUX(IB, 1: N / 2) AFLUX(IF, N / 2 + 1: N)]';
+        OUT = RM * IN + PM; 
+        AFLUX(IF, 1: N / 2) = OUT(1: N / 2);
+        AFLUX(IB, N / 2 + 1: N) = OUT(N / 2 + 1: N);
+      end
+    end
+    
+    % IN CASE OF BOUNDARY CONDITION ON THE RIGHT 
+    if (BC(2) == -1)
+      for m = 1: N/2
+        AFLUX(ntc + 1, N/2 + m) = AFLUX(ntc + 1, m);
+      end
+    end
+    
+    % 2. RIGHT -> LEFT SWEEP
+    IB = ntc + 1;
+    for r = NR: -1: 1
+      nc = DOM(2, r);
+      RM = R(:, :, r); PM = P(:, r);
+      for i = 1: nc
+        IB = IB - 1; IF = IB + 1;
+        IN = [AFLUX(IB, 1: N / 2) AFLUX(IF, N / 2 + 1: N)]';
+        OUT = RM * IN + PM; 
+        AFLUX(IF, 1: N / 2) = OUT(1: N / 2);
+        AFLUX(IB, N / 2 + 1: N) = OUT(N / 2 + 1: N);
+      end
+    end
+    
+    % IN CASE OF BOUNDARY CONDITION ON THE LEFT
+    if (BC(1) == -1)
+      for m = 1: N/2
+        AFLUX(1, m) = AFLUX(1, N/2 + m);
+      end
+    end
+    
+    % 3. STOP CRITERIA
+    IB = 0;
+    for r = 1: NR
+      nc = DOM(2, r);
+      for i = 1: nc
+        IB = IB + 1;
+        faux = SFLUX(IB);
+        sflux = 0;
+        for m = 1: N
+          w = QUAD(m,2); 
+          sflux = sflux + AFLUX(IB, m) * w;
+        end
+        sflux = 0.5*sflux;
+        SFLUX(IB) = sflux;
+        if (abs(1 - faux/sflux) > err)
+          err = abs(1 - faux/sflux);
+        end
+      end
+    end
+    
+    fprintf("%d\t\t\t%.5e\n", ITER, err);
+    
+  end
+  
+  TIME = toc;
+
+end
