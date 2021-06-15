d301db48ae2d39df940b4ce454b7888107e1108e
ibrivasortiz92 <78561871+ibrivasortiz92@users.noreply.github.com>
2021-06-15 14:35:26 -0300

    particular solution 2

--- METHODS_2D/MET_RM_LLN/test3.m
+++ METHODS_2D/MET_RM_LLN/test3.m
@@ -2,6 +2,7 @@
 
 M = N * (N + 2) / 2; I = sum(XDOM(2, :)); J = sum(YDOM(2, :));
 NZ = length(ZON(1,:));
+tol = 10e-10;
 
 A = zeros(M, M, NZ);
 for z = 1: NZ
@@ -18,10 +19,78 @@ for z = 1: NZ
   end
 end
 
+% PARTICULAR SOLUTION X0
+DFLUX0 = rand(M,1); DFLUX1 = rand(M,1);
+PSOL0 = zeros(M,1); PSOL1 = zeros(M,1);
+st = ZON(1,1); ss = ZON(2,1); c0 = ss/st; hi = rand(1); hj = rand(1);
+for m = 1: M
+  miu = QUAD(m,1); theta = QUAD(m,2); w = QUAD(m,3);
+  AUX0 = 0; AUX1 = 0; AUX2 = 0; AUX4 = 0;
+  for k = 1: M
+    kmiu = QUAD(k,1); ktheta = QUAD(k,2); kw = QUAD(k,3);
+    AUX0 = AUX0 + 0.25*c0*kw*ktheta*DFLUX0(k)/(st*hj*(1-c0));
+    AUX1 = AUX1 + 0.5*c0*miu*kw*ktheta*DFLUX1(k)/(st*hi*st*hj*(1-c0));
+    AUX2 = AUX2 + 0.5*c0*kw*kmiu*ktheta*DFLUX1(k)/(st*hi*st*hj*(1-c0));
+    AUX4 = AUX4 + 0.25*c0*kw*ktheta*DFLUX1(k)/(st*hj*(1-c0));
+  end
+  PSOL0(m) = -theta*DFLUX0(m)/(st*hj) - AUX0 + 2*miu*theta*DFLUX1(m)/(st*hi*st*hj) ...
+             + AUX1 + AUX2;
+  PSOL1(m) = -theta*DFLUX1(m)/(st*hj) - AUX4;
+end
+% TESTING PARTICULAR SOLUTION Y0
+for m = 1: M
+  miu = QUAD(m,1); theta = QUAD(m,2);
+  AUX0 = 0; AUX1 = 0;
+  for n = 1: M
+    nw = QUAD(n,3);
+    AUX0 = AUX0 + nw*PSOL0(n);
+    AUX1 = AUX1 + nw*PSOL1(n);
+  end
+  comp0 = 2*miu*PSOL1(m)/(st*hi) + PSOL0(m) - 0.25*c0*AUX0 + theta*DFLUX0(m)/(st*hj);
+  comp1 = PSOL1(m) - 0.25*c0*AUX1 + theta*DFLUX1(m)/(st*hj);
+  assert(comp0 < tol, 'Error: PSOL0 X0');
+  assert(comp1 < tol, 'Error: PSOL1 X0');
+end
+
+
+% PARTICULAR SOLUTION Y0
+DFLUX0 = rand(M,1); DFLUX1 = rand(M,1);
+PSOL0 = zeros(M,1); PSOL1 = zeros(M,1);
+st = ZON(1,1); ss = ZON(2,1); c0 = ss/st; hi = rand(1); hj = rand(1);
+for m = 1: M
+  miu = QUAD(m,1); theta = QUAD(m,2); w = QUAD(m,3);
+  AUX0 = 0; AUX1 = 0; AUX2 = 0; AUX4 = 0;
+  for k = 1: M
+    kmiu = QUAD(k,1); ktheta = QUAD(k,2); kw = QUAD(k,3);
+    AUX0 = AUX0 + 0.25*c0*kw*kmiu*DFLUX0(k)/(st*hi*(1-c0));
+    AUX1 = AUX1 + 0.5*c0*theta*kw*kmiu*DFLUX1(k)/(st*hi*st*hj*(1-c0));
+    AUX2 = AUX2 + 0.5*c0*kw*kmiu*ktheta*DFLUX1(k)/(st*hi*st*hj*(1-c0));
+    AUX4 = AUX4 + 0.25*c0*kw*kmiu*DFLUX1(k)/(st*hi*(1-c0));
+  end
+  PSOL0(m) = -miu*DFLUX0(m)/(st*hi) - AUX0 + 2*miu*theta*DFLUX1(m)/(st*hi*st*hj) ...
+             + AUX1 + AUX2;
+  PSOL1(m) = -miu*DFLUX1(m)/(st*hi) - AUX4;
+end
+% TESTING PARTICULAR SOLUTION Y0
+for m = 1: M
+  miu = QUAD(m,1); theta = QUAD(m,2);
+  AUX0 = 0; AUX1 = 0;
+  for n = 1: M
+    nw = QUAD(n,3);
+    AUX0 = AUX0 + nw*PSOL0(n);
+    AUX1 = AUX1 + nw*PSOL1(n);
+  end
+  comp0 = 2*theta*PSOL1(m)/(st*hj) + PSOL0(m) - 0.25*c0*AUX0 + miu*DFLUX0(m)/(st*hi);
+  comp1 = PSOL1(m) - 0.25*c0*AUX1 + miu*DFLUX1(m)/(st*hi);
+  assert(comp0 < tol, 'Error: PSOL0 Y0');
+  assert(comp1 < tol, 'Error: PSOL1 Y0');
+end
+
+
 % PARTICULAR SOLUTION
 [xvals, xvects, yvals, yvects] = SPECTRUM_XY(QUAD, chi, ZON);
 INV = inv(xvects);
-tol = 10e-10;
+
 st = 1; hj = 1;
 NABLA = zeros(M,M); F0 = zeros(M,M); F1 = zeros(M,M);
 for m = 1: M
