ae6a470e4994fc66498cbf981620f0af2d895f5d
ibrivasortiz92 <78561871+ibrivasortiz92@users.noreply.github.com>
2021-07-17 19:44:24 -0300

    response matrix part1

--- METHODS_2D/MET_RM_LLN/response_matrix.m
+++ METHODS_2D/MET_RM_LLN/response_matrix.m
@@ -2,7 +2,7 @@
 % ITERATIVE PROCESS
 
 % OUTPUT: R = RESPONSE MATRIX FOR THE ITERATIVE PROCESS
-%         P = SOURCE VECTOR FOR THE ITERATIVE PROCESS
+%         S = SOURCE VECTOR FOR THE ITERATIVE PROCESS
 %         RM = RESPONSE MATRIZ FOR THE RECONSTRUCTION OF THE AVERAGE
 %              ANGULAR FLUXES IN EACH NODE
 %         LM = LEAKAGE MATRIX FOR THE RECONTRUCTION OF THE AVERAGE ANGULAR
@@ -10,9 +10,9 @@
 %         PM = SOURCE VECTOR FOR THE RECONSTRUCTION OF THE AVERAGE ANGULAR
 %              FLUXES IN EACH NODE
 
-function [R, P, RM, LM, PM] = response_matrix(N, ZON, XDOM, YDOM, ZMAP, QMAP)
+function [R, S] = response_matrix(N, ZON, XDOM, YDOM, ZMAP, QMAP)
 % SHORTHAND
-% R(m, k, ry, rx)   P(m, ry, rx)
+% 
   
   % INITIALIZE VARIABLES
   NRX = length(XDOM(1, :)); NRY = length(YDOM(1, :));
@@ -21,137 +21,139 @@ function [R, P, RM, LM, PM] = response_matrix(N, ZON, XDOM, YDOM, ZMAP, QMAP)
   
   % RESPONSE MATRIX AND SOURCE VECTOR
   M = N * (N + 2) / 2;
-  R = zeros(2 * M, 2 * M, NRY, NRX); P = zeros(2 * M, NRY, NRX);
-  RM = zeros(M, M, NRY, NRX); LM = zeros(M, M, NRY, NRX);
-  PM = zeros(M, NRY, NRX);
-  
-  % AUXILIARY MATRICES
-  XA = zeros(M, M); YA = zeros(M, M); XB = zeros(M, M); YB = zeros(M, M);
-  XE = zeros(M, M); YE = zeros(M, M); I = eye(M, M);    H = zeros(M, 1);
-  XM = zeros(M, M);
+  R = zeros(4 * M, 4 * M, NRY, NRX); S = zeros(4 * M, NRY, NRX);
   
   % DEFINE EACH MATRIX PER REGION
   for ry = 1: NRY
     for rx = 1: NRX
+        
+      % AUXILIARY MATRICES NEW
+      RXIN = zeros(M, M); RXOUT = zeros(M, M); SX = zeros(M,1);
+      FX0 = zeros(M, M); FX1IN = zeros(M, M); FX1OUT = zeros(M, M);
+      AX0 = zeros(M, M); AX1_PLUS = zeros(M, M); AX1_MINUS = zeros(M, M);
     
-      % FILL AUXILIARY MATRICES
+      % AUXILIARY CONSTANTS
       lenx = XDOM(1, rx); ntcx = XDOM(2, rx); hx = lenx / ntcx;
       leny = YDOM(1, ry); ntcy = YDOM(2, ry); hy = leny / ntcy;
       z = ZMAP(ry, rx); st = ZON(1, z); ss = ZON(2, z); c0 = ss/st;
       Q = QMAP(ry, rx);
+      
+      % DIRECTION LOOP
       for m = 1: M
         
-        % XA, YA, XE, YE MATRICES
+        m_miu = QUAD(m, 1); m_theta = QUAD(m, 2); m_w = QUAD(m, 3);
+          
+        % PRIMARY MATRICES
         if (m <= M / 4) % ROWS
+            
           for k = 1: M  % COLUMNS
-            XA(m, k) = xvects(m, k, z) * exp(0.5 * st * hx / xvals(k, z));
-            XE(m, k) = xvects(m, k, z) * exp(- 0.5 * st * hx / xvals(k, z));
+            RXOUT(m, k) = xvects(m, k, z) * exp(0.5 * st * hx / xvals(k, z));
+            RXIN(m, k) = xvects(m, k, z) * exp(- 0.5 * st * hx / xvals(k, z));
             
             YA(m, k) = yvects(m, k, z) * exp(0.5 * st * hy / yvals(k, z));
             YE(m, k) = yvects(m, k, z) * exp(- 0.5 * st * hy / yvals(k, z));
-            
           end
+          
         elseif (m > M / 4 && m <= M / 2) % ROWS
+            
           for k = 1: M % COLUMNS
-            XA(m, k) = xvects(m, k, z) * exp(- 0.5 * st * hx / xvals(k, z));
-            XE(m, k) = xvects(m, k, z) * exp(0.5 * st * hx / xvals(k, z));
+            RXOUT(m, k) = xvects(m, k, z) * exp(- 0.5 * st * hx / xvals(k, z));
+            RXIN(m, k) = xvects(m, k, z) * exp(0.5 * st * hx / xvals(k, z));
             
             YA(m, k) = yvects(m, k, z) * exp(0.5 * st * hy / yvals(k, z));
             YE(m, k) = yvects(m, k, z) * exp(- 0.5 * st * hy / yvals(k, z));
-            
           end
+          
         elseif (m > M / 2 && m <= 3 * M / 4) % ROWS
+            
           for k = 1: M % COLUMNS
-            XA(m, k) = xvects(m, k, z) * exp(- 0.5 * st * hx / xvals(k, z));
-            XE(m, k) = xvects(m, k, z) * exp(0.5 * st * hx / xvals(k, z));
+            RXOUT(m, k) = xvects(m, k, z) * exp(- 0.5 * st * hx / xvals(k, z));
+            RXIN(m, k) = xvects(m, k, z) * exp(0.5 * st * hx / xvals(k, z));
             
             YA(m, k) = yvects(m, k, z) * exp(- 0.5 * st * hy / yvals(k, z));
             YE(m, k) = yvects(m, k, z) * exp(0.5 * st * hy / yvals(k, z));
-            
           end
+          
         elseif (m > 3 * M / 4 && m <= M) % ROWS
+            
           for k = 1: M % COLUMNS
-            XA(m, k) = xvects(m, k, z) * exp(0.5 * st * hx / xvals(k, z));
-            XE(m, k) = xvects(m, k, z) * exp(- 0.5 * st * hx / xvals(k, z));
+            RXOUT(m, k) = xvects(m, k, z) * exp(0.5 * st * hx / xvals(k, z));
+            RXIN(m, k) = xvects(m, k, z) * exp(- 0.5 * st * hx / xvals(k, z));
             
             YA(m, k) = yvects(m, k, z) * exp(- 0.5 * st * hy / yvals(k, z));
             YE(m, k) = yvects(m, k, z) * exp(0.5 * st * hy / yvals(k, z));
-            
           end
+          
         end
         
-        % XB, YB MATRICES
+        % SOURCE MATRICES
+        SX(m) = Q / (st * (1 - c0));
+        
+        % SECONDARY MATRICES
         for k = 1: M % COLUMNS
-          miu = QUAD(k, 1); theta = QUAD(k, 2); w = QUAD(k, 3);
-          if (k <= M / 4)
-            XB(m, k) = - 0.25 * c0 * theta * w / (st * hy * (1 - c0));
-            if (k == m), XB(m, k) = XB(m,k) - theta / (st * hy); end
-         
-            YB(m, k) = - 0.25 * c0 * miu * w / (st * hx * (1 - c0));
-            if (k == m), YB(m, k) = YB(m,k) - miu / (st * hx); end
-             
-          elseif (k > M / 4 && k <= M / 2)
-            XB(m, k) = - 0.25 * c0 * theta * w / (st * hy * (1 - c0));
-            if (k == m), XB(m, k) = XB(m,k) - theta / (st * hy); end
-             
-            YB(m, k) = 0.25 * c0 * miu * w / (st * hx * (1 - c0));
-            if (k == m), YB(m, k) = YB(m,k) + miu / (st * hx); end
-              
-          elseif (k > M / 2 && k <= 3 * M / 4)
-            XB(m, k) = 0.25 * c0 * theta * w / (st * hy * (1 - c0));
-            if (k == m), XB(m, k) = XB(m,k) + theta / (st * hy); end
-              
-            YB(m, k) = 0.25 * c0 * miu * w / (st * hx * (1 - c0));
-            if (k == m), YB(m, k) = YB(m,k) + miu / (st * hx); end
-              
-          elseif (k > 3 * M / 4 && k <= M)
-            XB(m, k) = 0.25 * c0 * theta * w / (st * hy * (1 - c0));
-            if (k == m), XB(m, k) = XB(m,k) + theta / (st * hy); end
-              
-            YB(m, k) = - 0.25 * c0 * miu * w / (st * hx * (1 - c0));
-            if (k == m), YB(m, k) = YB(m,k) - miu / (st * hx); end
-              
+            
+          k_miu = QUAD(k, 1); k_theta = QUAD(k, 2); k_w = QUAD(k, 3);
+          
+          AX0(m,k) = - 0.25 * c0 * k_w * k_theta / (st * hy * (1 - c0));
+          AX1_PLUS(m,k) = 0.5 * c0 * k_w * k_theta * (k_miu / (st * hx) + m_miu / (st * hx) + 0.5) / (st * hy * (1 - c0));
+          AX1_MINUS(m,k) = 0.5 * c0 * k_w * k_theta * (k_miu / (st * hx) + m_miu / (st * hx) - 0.5) / (st * hy * (1 - c0));
+          if (k == m)
+              AX0(m,k) = AX0(m,k) - m_theta / (st * hy);
+              AX1_PLUS(m,k) = AX1_PLUS(m,k) + 2 * m_theta * (m_miu / (st * hx) + 0.5) / (st * hy);
+              AX1_MINUS(m,k) = AX1_MINUS(m,k) + 2 * m_theta * (m_miu / (st * hx) - 0.5) / (st * hy);
           end
           
-          XM(m, k) = xvects(m, k, z) * xvals(k, z) * 2 * sinh(0.5 * st * hx / xvals(k, z)) / (st * hx);
+          if (k <= M / 4)
+            FX0(m,k) = AX0(m,k);
+            FX1IN(m,k) = AX1_PLUS(m,k);
+            FX1OUT(m,k) = AX1_MINUS(m,k);
+          elseif (k > M / 4 && k <= M / 2)
+            FX0(m,k) = AX0(m,k);
+            FX1IN(m,k) = AX1_MINUS(m,k);
+            FX1OUT(m,k) = AX1_PLUS(m,k);
+          elseif (k > M / 2 && k <= 3 * M / 4)
+            FX0(m,k) = - AX0(m,k);
+            FX1IN(m,k) = - AX1_MINUS(m,k);
+            FX1OUT(m,k) = - AX1_PLUS(m,k);
+          elseif (k > 3 * M / 4 && k <= M)
+            FX0(m,k) = - AX0(m,k);
+            FX1IN(m,k) = - AX1_PLUS(m,k);
+            FX1OUT(m,k) = - AX1_MINUS(m,k);
+          end
           
         end
-        
-        % H COLUMN VECTOR
-        H(m) = Q / (st * (1 - c0));
         
       end
-    
-      XE_INV = inv(XE); 
-      YE_INV = inv(YE);
+          
+      ZERO = zeros(M, M);
+      IDEN = eye(M, M);
+      RXIN_INV = inv(RXIN);
       
-      % CALCULATE THE RESPONSE MATRIX FOR RECONSTRUCTION
-      RM(:, :, ry, rx) = XM * XE_INV;
+      % EQUATION 1
+      MX1 = RXOUT * RXIN_INV;
+      MX2 = FX0 - (RXOUT * RXIN_INV) * FX0;
+      MX4 = FX1OUT - (RXOUT * RXIN_INV) * FX1IN;
+      MX5 = (IDEN - RXOUT * RXIN_INV) * SX;
       
-      % CALCULATE THE LEAKAGE MATRIX FOR RECONSTRUCTION
-      LM(:, :, ry, rx) = XB - XM * (XE_INV * XB);
-      
-      % CALCULATE THE SOURCE VECTOR FRO RECONSTRUCTION
-      PM(:, ry, rx) = (I - XM * XE_INV) * H;
-      
-      AUX = [           I                 , (- (XB - XA * (XE_INV * XB)));
-             (- (YB - YA * (YE_INV * YB))),              I              ];
-      
-      AUX_INV = inv(AUX);
-      
-      AUX2 = [        (XA * XE_INV)        , (- (XB - XA * (XE_INV * XB)));
-              (- (YB - YA * (YE_INV * YB))),       (YA * YE_INV)       ];
-      
+      IN = [MX1,   -MX2,  ZERO,  -MX4;
+            -MY1,  MY2,   -MY3,  ZERO;
+            MXX1,  -MXX2, MXX3,  -MXX4;
+            -MYY1, MYY2,  -MYY3, MYY4];
+          
+      OUT = [IDEN,  -MX2,  ZERO,  -MX4;
+             -MY1,  IDEN,  -MY3,  ZERO;
+             ZERO,  -MXX2, IDEN,  -MXX4;
+             -MYY1, ZERO,  -MYY3, IDEN];
+         
+      OUT_INV = inv(OUT);
+          
       % CALCULATE THE RESPONSE MATRIX
-      R(:, :, ry, rx) = AUX_INV * AUX2;
+      R(:, :, ry, rx) = OUT_INV * IN;
       
-      AUX3 = [(I - XA * XE_INV),    zeros(M, M)    ;
-                zeros(M, M)    , (I - YA * YE_INV)];
-      
-      S = [H; H];
+      IND = [MX5; MY5; MXX5; MYY5];
       
       % CALCULATE THE SOURCE VECTOR
-      P(:, ry, rx) = AUX_INV * (AUX3 * S);
+      S(:, ry, rx) = OUT_INV * IND;
     
     end
   end
